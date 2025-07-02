0858664e05dc05dd16f144ccee5b89d1e6b6aff2
mellinda009 <132473744+mellinda009@users.noreply.github.com>
2025-07-01 21:34:52 -0300

    01/06

--- script.js
+++ script.js
@@ -24,15 +24,43 @@ function aumentaTamanho() {
 }
 
 const campoSenha = document.querySelector('#campo-senha');
+
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'ancdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789'
+const simbolos = '!@%*?';
+
+const checkbox = document.querySelectorAll('.checkbox')
+
+function geraSenha() {
+ let alfabeto = '' ;
+ if (checkbox [0].checkbox) {
+    alfabeto = alfabeto + letrasMaiusculas;
+ }
+ if (checkbox [1].checkbox) {
+    alfabeto = alfabeto + letrasMinusculas;
+ }
+ if (checkbox [2].checkbox) {
+    alfabeto = alfabeto + numeros;
+ }
+ if (checkbox [3].checkbox) {
+    alfabeto = alfabeto + simbolos;
+ }
+
+ console.log(alfabeto);
+    let senha= '';
+    for(let i=0; i < tamanhoSenha; i++){
+    numeroAleatorio = Math.random()*alfabeto.length;
+    senha = senha + alfabeto[numeroAleatorio]
+    }
+     
+}
+
 geraSenha();
 
 function geraSenha() {
-    let senha = '' ;
-    for(let i=0; i < tamanhoSenha; i++) {
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-        numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas[numeroAleatorio];
-    }
-    campoSenha.value = senha; 
+
+    for(let i=0; i < checklength; i++;)
+        {
+        
 }
