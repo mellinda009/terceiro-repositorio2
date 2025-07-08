a591a93949dd5f1cf0e84037ccc0fa3b8e416501
mellinda009 <132473744+mellinda009@users.noreply.github.com>
2025-07-08 20:10:26 -0300

    08/07

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
+
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
@@ -7,15 +8,16 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho() {
-    if (tamanhoSenha > 1) {
+function diminuiTamanho(){
+    //tamanhoSenha = tamanhoSenha-1;
+    if (tamanhoSenha > 1){
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 
-function aumentaTamanho() {
+function aumentaTamanho(){
     if (tamanhoSenha < 20){
         tamanhoSenha++;
     }
@@ -23,31 +25,30 @@ function aumentaTamanho() {
     geraSenha();
 }
 
-const campoSenha = document.querySelector('#campo-senha');
+const campoSenha = document.querySelector("#campo-senha");
 
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letrasMinusculas = 'ancdefghijklmnopqrstuvwxyz';
-const numeros = '0123456789'
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
 const simbolos = '!@%*?';
 
-const checkbox = document.querySelectorAll('.checkbox')
+const checkbox = document.querySelectorAll('.checkbox');
 
 function geraSenha() {
- let alfabeto = '' ;
- if (checkbox [0].checkbox) {
-    alfabeto = alfabeto + letrasMaiusculas;
- }
- if (checkbox [1].checkbox) {
-    alfabeto = alfabeto + letrasMinusculas;
- }
- if (checkbox [2].checkbox) {
-    alfabeto = alfabeto + numeros;
- }
- if (checkbox [3].checkbox) {
-    alfabeto = alfabeto + simbolos;
- }
-
- console.log(alfabeto);
+    let alfabeto = '';
+    if (checkbox[0].checked) {
+        alfabeto =alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked) {
+        alfabeto =alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked) {
+        alfabeto =alfabeto + numeros;
+    }
+    if (checkbox[3].checked) {
+        alfabeto =alfabeto + simbolos;
+    }
+    console.log(alfabeto);
     let senha = '';
     for (let i=0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random()*alfabeto.length;
