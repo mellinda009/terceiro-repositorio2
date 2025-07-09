6f8fe30cfa3374974ab8a87382c49e512eaa2436
mellinda009 <132473744+mellinda009@users.noreply.github.com>
2025-07-08 21:16:53 -0300

    aula 8

--- script.js
+++ script.js
@@ -56,10 +56,22 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
+    classificaSenha(alfabeto.length);
 }
 
 geraSenha();
 
-for (i=0; i< checkbox.length; i++) {
-    checkbox[i].onclick = geraSenha;
+function classificaSenha(tamanhoAlfabeto) {
+ let entropia = tamanhoSenha*Math.log2(tamanhoAlfabeto);
+ console.log(entropia);
+ forcaSenha.classlist.remove('fraca', 'media','forte');
+    if (entropia > 57) {
+        forcaSenha.classlist.add('forte');
+    } else if (entropia > 35 && entropia < 57) {
+         forcaSenha.classlist.add('media');
+    } else if (entropia <= 35) {
+        forcaSenha.classlist.add('fraca');
+    }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
 }
